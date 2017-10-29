6e9f389d8ebb7f0f92fdd1154b0468fd73f7cd13
Alisahhh <923431911@qq.com>
2017-10-29 13:16:19 +0800

    add Travel

--- Traversal.cpp
+++ Traversal.cpp
@@ -2,19 +2,46 @@
 #include<iostream>
 #include<cstring>
 #include<io.h>
-void Travel(char name[]);
 
+void Travel(string Path);
+void Read(string Path);
 int main()
 {
 	using namespace std;
-	char FilePath[105];
+	string FilePath;
 	cin >> FilePath;
 	Travel(FilePath);
 
 }
 
-void Travel(char Path[])
+void Travel(string Path)
+{
+	_ finddata_t File;
+	string NowPath = Path + "/*";
+	long Handle = _findfirst(NowPath.c_str(),&File);
+	if(Handle == -1) 
+	{
+		cout<<"No File"<<endl;
+		return;
+	}
+	while(!_findnext(handle,&File))
+	{
+		if(File.atteib == _A_SUBDIR)//folder
+		{
+			if(strcmp(File.name,"..") != 0&&strcmp(File.name,".")!=0)
+			{
+				Travel(Path+'l'+File.name);
+			}
+		}
+		else
+		{
+			Read(Path+'l'+File.name);
+		}
+	}
+	_findclose(handle);
+}
+
+void Read(string File)
 {
 	
 }
-
