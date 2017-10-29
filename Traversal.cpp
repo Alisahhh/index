69fdfd4e6a0e6fbf9ee8496eb4e7ff9c5fc503e6
Alisahhh <923431911@qq.com>
2017-10-29 10:45:25 +0800

    add Traversal.cpp

---
+++ Traversal.cpp
@@ -0,0 +1,20 @@
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include<io.h>
+void Travel(char name[]);
+
+int main()
+{
+	using namespace std;
+	char FilePath[105];
+	cin >> FilePath;
+	Travel(FilePath);
+
+}
+
+void Travel(char Path[])
+{
+	
+}
+
