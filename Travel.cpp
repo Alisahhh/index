5e0ce2b7a475b339df2f179ec87c826658a14290
Alisahhh <923431911@qq.com>
2017-10-31 20:44:01 +0800

    index done

--- Travel.cpp
+++ Travel.cpp
@@ -10,49 +10,39 @@
 #include <queue>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <time.h>
 #include <vector>
 using namespace std;
 
 struct SS {
   string word;
-  int wordId;
+  int fileNum;
+  int count;
 };
-vector<vector<int>> inWhichFile;
-vector<SS> word;            // 每个单词所对应的编号
+vector<SS> sum;
+vector<string> allFile;     // 每个单词在每个文件里的数量
 map<string, int> fileStore; //储存文件编号
 map<string, int> wordStore; //确定单词是否被读以及赋予编号
-int wordCount, fileCount;   //单词总数，文件总数
-
-void makeWord(string S, int fileCount) {
-  if (!wordStore[S]) {
-    wordCount++;
-    wordStore[S] = wordCount;
-    word.push_back(SS{S, wordCount});
-  }
-  // cout << S << " " << fileCount << " " << wordCount << endl;
-  // inWhichFile[wordStore[S]].push_back(fileCount);
-}
-
+map<int, int> sumInAFile;
+int wordCount, fileCount; //单词总数，文件总数
 void scan_file(const char *fileName) {
   fileCount++;
+  sumInAFile.clear();
+  wordStore.clear();
+  wordCount = 0;
   string _filename(fileName);
-  cout << _filename << endl;
   fileStore[_filename] = fileCount;
-  cout << fileCount << endl;
+  allFile.push_back(_filename);
   ifstream infile;
   infile.open(fileName);
-  printf("OK\n");
   string S;
   while (infile >> S) {
-    cout << S << endl;
-    makeWord(S, fileCount);
+    if (!wordStore[S])
+      wordStore[S] = ++wordCount;
+    sumInAFile[wordStore[S]]++;
+    sum.push_back(SS{S, fileCount, sumInAFile[wordStore[S]]});
   }
-  //}
   infile.close();
-  for (int i = 0; i < word.size(); i++) {
-    SS tmp = word[i];
-    cout << tmp.word << " " << tmp.wordId;
-  }
 }
 
 void scan_dir(const char *path) {
@@ -66,7 +56,6 @@ void scan_dir(const char *path) {
     string _dirName(ent->d_name);
     string _fullPath = _path + "/" + _dirName;
     if (ent->d_type == 8) {
-      cout << ent->d_name << endl;
       scan_file(_fullPath.c_str());
     } else {
       if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)
@@ -74,23 +63,41 @@ void scan_dir(const char *path) {
       scan_dir(_fullPath.c_str());
     }
   }
-  cout << fileCount << endl;
 }
 
-bool cmp(SS a, SS b) {
-  if (a.word < b.word)
-    return 1;
-}
+bool cmp(SS a, SS b) { return a.word < b.word; }
 
 void printIndex() {
-  sort(word.begin(), word.end(), cmp);
-  for (int i = 1; i <= wordCount; i++) {
-    cout << word[i].word << " ";
-    for (int j = 0; j < inWhichFile.size(); j++) {
-      cout << inWhichFile[i][j] << " ";
+  ofstream outfile;
+  outfile.open("index.dat");
+  wordStore.clear();
+  sumInAFile.clear();
+  wordCount = 0;
+  outfile << fileCount << endl;
+  for (int i = 0; i < allFile.size(); i++)
+    outfile << allFile[i] << " " << fileStore[allFile[i]] << endl;
+  sort(sum.begin(), sum.end(), cmp);
+  for (int i = 0; i < sum.size(); i++) {
+    if (!wordStore[sum[i].word]) {
+      wordCount++;
+      wordStore[sum[i].word] = 1;
     }
-    cout << "\n";
+    sumInAFile[wordCount]++;
   }
+  outfile << wordCount << endl;
+  for (int i = 1; i <= wordCount; i++)
+    outfile << sumInAFile[i] << " ";
+  outfile << " ";
+  wordStore.clear();
+  for (int i = 0; i < sum.size(); i++) {
+    if (!wordStore[sum[i].word]) {
+      outfile << endl;
+      wordStore[sum[i].word] = 1;
+      outfile << sum[i].word << " ";
+    }
+    outfile << sum[i].fileNum << " " << sum[i].count << " ";
+  }
+  outfile.close();
 }
 
 int main() {
@@ -98,6 +105,7 @@ int main() {
   cin >> path;
   cout << path;
   scan_dir(path.c_str());
-  // scan_dir("/home/alisa/Documents/code/tmp");
-  // printIndex();
+  cout << "done" << endl;
+  cout << "Totle Time : " << (double)clock() / CLOCKS_PER_SEC << "s" << endl;
+  printIndex();
 }
